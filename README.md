d0159e56326eb8aab0c30414c28c9e522735fefd
Loek Linnemans <144667567+LoekLinnemans@users.noreply.github.com>
2024-03-11 09:25:32 +0100

    better error handling

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # diskuser
+
+result will be put in the resulkt.txt file.
+
+
+os exit status
+1 
+unable to create a file.(check error.log for more information)
+
+2
+error when trying to find the directory.(check error.log for more information)
+
+3
+error when trying to write to file.(check error.log for more information)
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func init() {
 	logFile, err := os.OpenFile("error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Println("couldn't create logfile")
+		log.Println("couldn't create logfile", err)
 		os.Exit(1)
 	}
 
@@ -20,32 +20,50 @@ func init() {
 
 }
 
+func UserInput() string {
+
+	var input string
+
+	fmt.Println("enter the directory to scan:")
+
+	fmt.Scanln(&input)
+	return input
+}
+
 func ScanFiles(dir string) {
+	var totalSize int64
+
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Println("Error finding directory or file: ", err)
+			os.Exit(2)
 			return err
 		}
-		WriteToFile(fmt.Sprintf("Path: %v, Name: %v, Size: %v bytes\n", path, info.Name(), info.Size()))
+		if info.IsDir() {
+			totalSize += info.Size()
+		}
 		return nil
 	})
+	WriteToFile(fmt.Sprintf("Memory Usage of %s: %v bytes\n", dir, totalSize))
 }
 
 func WriteToFile(dir string) {
 	file, err := os.OpenFile("result.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Println("Error creating file:", err)
+		log.Println("Error creating result.txt:", err)
+		os.Exit(1)
 		return
 	}
 	_, err = file.WriteString(dir)
 	if err != nil {
 		log.Println("Error writing to file:", err)
+		os.Exit(3)
 		return
 	}
 	defer file.Close()
 }
 
 func main() {
-	directory := "C:\\Users\\loek\\OneDrive\\Documenten\\World Machine Documents"
+	directory := UserInput()
 	ScanFiles(directory)
 }
